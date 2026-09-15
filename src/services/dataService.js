fb1e31194e73c6f8505c0ae1a06bcced157a28a6
MD Muntasir Alam Resti <muntasiralamresti@gmail.com>
2026-09-15 15:07:14 +0600

    Migrate data service to AsyncStorage

--- src/services/dataService.js
+++ src/services/dataService.js
@@ -1,21 +1,11 @@
-import { initDB } from '../database/db';
+import { initDB, saveDB } from '../database/db';
 
-export const getCategories = async () => {
-  const db = await initDB();
-  return db.getAll('categories');
-};
-
-export const getPaymentMethods = async () => {
-  const db = await initDB();
-  return db.getAll('paymentMethods');
-};
-
-export const getSettings = async () => {
-  const db = await initDB();
-  return db.get('settings', 'preferences');
-};
-
+export const getCategories = async () => (await initDB()).categories;
+export const getPaymentMethods = async () => (await initDB()).paymentMethods;
+export const getSettings = async () => (await initDB()).settings;
 export const updateSettings = async (settings) => {
   const db = await initDB();
-  await db.put('settings', { ...settings, id: 'preferences' });
+  db.settings = { ...db.settings, ...settings, id: 'preferences' };
+  await saveDB(db);
+  return db.settings;
 };
